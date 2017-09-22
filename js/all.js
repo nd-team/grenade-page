cd08df702df018802bc3478217c0645d59b12e44
wangruyi007 <122979625@qq.com>
2017-09-22 20:33:53 +0800

    官网

---
+++ js/all.js
@@ -0,0 +1,167 @@
+$('.success-right').click(function () {
+    var current = $('.success-pagination  a.pag-cur').index();
+    var reconLen = $('.success-pagination  a').length;
+    current++;
+    if (current >= reconLen) {
+        current = 0;
+    }
+    $('.success-pagination a').removeClass('pag-cur');
+    $('.success-pagination a').eq(current).addClass('pag-cur');
+    $('.success .su-items').removeClass('show');
+    $('.success .su-items').eq(current).addClass('show');
+});
+$('.success-left').click(function () {
+    var current = $('.success-pagination  a.pag-cur').index();
+    var reconLen = $('.success-pagination  a').length;
+    current--;
+    if (current == -1) {
+        current = reconLen-1;
+    }
+    $('.success-pagination a').removeClass('pag-cur');
+    $('.success-pagination a').eq(current).addClass('pag-cur');
+    $('.success .su-items').removeClass('show');
+    $('.success .su-items').eq(current).addClass('show');
+});
+$('.success-pagination a').click(function () {
+    var curs = $(this).index();
+    $('.success-pagination a').removeClass('pag-cur');
+    $(this).addClass('pag-cur');
+    $('.success .su-items').removeClass('show');
+    $('.success .su-items').eq(curs).addClass('show');
+});
+/*var auto=0;
+$(".he-left").click(function(){
+    auto++;
+    var len=$(".partner ul li").length;
+    if(auto+6>len){
+        $(".partner-cont ul").stop().append($(".partner-cont ul").html());
+    }
+    $(".partner-cont ul").stop().animate({left:-auto*188},1000);
+});
+$(".he-right").click(function(){
+    if(auto==0){
+        $(".partner-cont ul").prepend($(".partner-cont ul").html());
+        $(".partner-cont ul").css("left","-1380px");
+        auto=6
+    }
+    auto--;
+    $(".partner-cont ul").stop().animate({left:-auto*188},1000);
+});
+$(".partner-cont").hover(function() {
+    clearInterval(picTimer);
+},function() {
+    picTimer = setInterval(function() {
+        var len=$(".partner ul li").length;
+        auto++;
+        if(auto+6>len){
+            $(".partner-cont ul").stop().append($(".partner-cont ul").html());
+        }
+        $(".partner-cont ul").stop().animate({left:-auto*188},1000);
+        if(auto == len) {index = auto;}
+    },4000);
+}).trigger("mouseleave");*/
+//合作伙伴
+$('.swf p a:first').css({'background' : '#3d96ef'});
+function next(cl,mo){
+    var a;
+    var b;
+    $('.' + cl).find('ul').stop().animate({'margin-left' : mo},function(){
+        b = $(this).find('li').attr('class');
+        $('.swf p a.' + b).css({'background' : '#23344b'});
+        $(this).find('li:first').appendTo(this);
+        a = $(this).find('li:first').attr('class');
+        $(this).css({'margin-left' : '0'});
+        $('.swf p a.' + a).css({'background' : '#3d96ef'});
+    });
+}
+function prev(cl,mo){
+    $('.' + cl + ' ul li:last').prependTo('.' + cl + ' ul');
+    $('.' + cl + ' ul').css({'margin-left' : mo});
+    $('.' + cl + ' ul').stop().animate({'margin-left' : '0'})
+}
+var swf = setInterval(function(){
+    next('swf','-100%');
+},3000);
+$('.swf').mouseover(function(){
+    clearInterval(swf);
+});
+$('.swf').mouseleave(function(){
+    swf = setInterval(function(){
+        next('swf','-100%');
+    },3000);
+    var clear = setInterval(function(){
+        $('.swf p a').css({'background' : '#23344b'});
+        clearInterval(clear);
+    },3000);
+});
+$('.swf p a').click(function(e){
+    e.preventDefault();
+    var a;
+    var b;
+    a = $(this).attr('class');
+    b = $('.swf ul li.' + a).index();
+    $('.swf p a').css({'background' : '#23344b'});
+    $(this).css({'background' : '#060'});
+    $('.swf ul').stop().animate({'margin-left' : -100*b + '%'},function(){
+    });
+
+
+
+});
+function prev(cl,mo){
+    $('.' + cl + ' ul li:last').prependTo('.' + cl + ' ul');
+    $('.' + cl + ' ul').css({'margin-left' : mo});
+    $('.' + cl + ' ul').stop().animate({'margin-left' : '0'})
+}
+$('.he-left').click(function(){
+    next('flish','-176px');
+});
+$('.he-right').click(function(){
+    prev('flish','-176px')
+});
+
+$('.inactive').click(function(){
+    if($(this).siblings('ul').css('display')=='none'){
+        $(this).siblings('ul').slideDown(100).children('li');
+    }else{
+        $(this).removeClass('store-inactive');
+        $(this).siblings('ul').slideUp(100);
+    }
+});
+$('.ifs').find("em:nth-of-type(1)").hide();
+$('.table-body li .body-first').click(function () {
+    $(this).find(".ifs em").toggle();
+    $(this).next(".body-second").animate({height: 'toggle', opacity: 'toggle'}, "slow");
+    $(this).css("background-color","#f3f3f3");
+});
+$('#modal').hide();
+$('.apply').click(function () {
+    $('#modal').show();
+    $('#modalbg').show();
+});
+$('.close').click(function () {
+    $('#modal').hide();
+    $('#modalbg').hide();
+});
+$('.up-button').click(function () {
+    $('.upload').hide();
+    $('.upload2').show();
+});
+$('.news-right ul li a').click(function () {
+    if($(this).next(".cen-content").css('display')=='none'){
+        $(this).addClass('circle')
+    }else{
+        $(this).removeClass('circle')
+    }
+    $(this).next(".cen-content").animate({height: 'toggle', opacity: 'toggle'}, "slow");
+});
+$('.case-sec').click(function () {
+    $(this).next(".cur-son").animate({height: 'toggle', opacity: 'toggle'}, "slow");
+});
+$('.del').click(function (){
+     $(".upload").show();
+     $(".upload2").hide();
+});
+
+
+
